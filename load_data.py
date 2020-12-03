36ffea00089bd0c036fb0f63d6fa3e1bec629238
Jonathan Gantner <joanthan.gantner@gmx.at>
2020-12-03 09:01:08 +0100

    Loading of data per age_group and sex implemented Loading of data per community implemented

--- load_data.py
+++ load_data.py
@@ -79,6 +79,57 @@ def load_ages_capacity_data(start: dt.date, end: dt.date,
     return data
 
 
+def load_ages_cases_by_age_and_sex(file: str = os.path.join(config.ages_dir, 'CovidFaelle_Altersgruppe.csv')
+                                   ) -> pd.DataFrame:
+    """
+    Loads the current numbers of total infected, recovered and died cases in Austria by age group and sex
+    from the AGES file CovidFallzahlen.csv.
+
+    :param file:
+    :return: pd.DataFrame with columns 'age_group_id', 'age_group', 'region', 'region_id', 'population', 'sex',
+        'total', 'recovered', and 'dead'
+    """
+
+    # ToDo: MultiIndex Date + Region?
+    rename_map = {
+        'AltersgruppeID': 'age_group_id',
+        'Altersgruppe': 'age_group',
+        'Bundesland': 'region',
+        'BundeslandID': 'region_id',
+        'AnzEinwohner': 'population',
+        'Geschlecht': 'sex',
+        'Anzahl': 'total',
+        'AnzahlGeheilt': 'recovered',
+        'AnzahlTot': 'dead'
+    }
+
+    data = pd.read_csv(file, header=0, sep=';').rename(columns=rename_map)
+    return data
+
+
+def load_ages_cases_by_community(file: str = os.path.join(config.ages_dir, 'CovidFaelle_GKZ.csv')
+                                 ) -> pd.DataFrame:
+    """
+    Loads the current numbers of total active, recovered and died cases by community in Austria from the AGES file
+    CovidFallzahlen.csv.
+
+    :param file:
+    :return: pd.DataFrame with columns 'community', 'community_id', 'population', 'total', 'dead', 'cases_7days'
+    """
+    # ToDo: MultiIndex Date + Region?
+    rename_map = {
+        'Bezirk': 'community',
+        'GKZ': 'community_id',
+        'AnzEinwohner': 'population',
+        'Anzahl': 'total',
+        'AnzahlTot': 'dead',
+        'AnzahlFaelle7Tage': 'cases_7days'
+    }
+
+    data = pd.read_csv(file, header=0, sep=';').rename(columns=rename_map)
+    return data
+
+
 if __name__ == '__main__':
     # download_ages_data()
     # start = dt.date(day=1, month=7, year=2020)
@@ -90,3 +141,9 @@ if __name__ == '__main__':
 
     data_hosp = load_ages_capacity_data(start, end)
     print(data_hosp)
+
+    cases_age_and_sex = load_ages_cases_by_age_and_sex()
+    print(cases_age_and_sex)
+
+    cases_community = load_ages_cases_by_community()
+    print(cases_community)
